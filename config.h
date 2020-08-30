475917ba413f411693909c6b16420e8471e3500a
Max Schillinger <maxschillinger@web.de>
2020-08-30 23:07:42 +0200

    undo patch 'focusonclick' (because it prevents moving and resizing floating windows)

--- config.h
+++ config.h
@@ -12,7 +12,6 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheel       = 1;
 static const int viewontag          = 1;        /* Switch view on tag switch */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
