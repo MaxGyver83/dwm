82183f5a4d256652add4abf6b8a0fcde6fb4349d
Max Schillinger <maxschillinger@web.de>
2021-01-23 18:04:39 +0100

    add rule for Qalculate

--- config.h
+++ config.h
@@ -40,10 +40,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Alacritty",NULL,       "Calendar", 0,            1,           -1 },
+	/* class                 instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",                NULL,       NULL,       0,            1,           -1 },
+	{ "Qalculate",           NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",             NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Alacritty",           NULL,       "Calendar", 0,            1,           -1 },
+	/* { "VirtualBox Machine",  NULL,       NULL,       1 << 2,       0,           -1 }, */
 };
 
 /* layout(s) */
