ccf13b63c4c30b6eba3a3970572010f857bca712
Maximilian Schillinger <maximilian.schillinger@daimler.com>
2021-02-01 14:47:43 +0100

    change font from 'monospace' to 'terminus'

--- config.h
+++ config.h
@@ -17,7 +17,8 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const char *fonts[]          = { "monospace:size=14" };
+/* static const char *fonts[]          = { "monospace:size=14" }; */
+static const char *fonts[]          = { "terminus:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
